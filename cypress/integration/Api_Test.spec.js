dcc46fce9aae017e033243932153cf9f1bf1f833
Fabiana Alves <faads1991@gmail.com>
2022-09-19 19:40:07 -0300

    API

---
+++ cypress/integration/Api_Test.spec.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress"/>
+
+const { resolvePtr } = require("dns")
+
+describe('Requisição GraphQL', () => {
+   
+    it('Faz requisição GraphQL', () => {
+        var queryString = `{
+            getCityByName(
+                name: "Sao Paulo"
+                 ){
+                   country
+                   weather{
+                     summary{
+                       title
+                       description
+                     }
+                     temperature{
+                       actual
+                       feelsLike
+                       min
+                       max
+                     }
+                     wind{
+                       speed
+                     }
+                     clouds {
+                       all
+                       visibility
+                       humidity
+                     }
+                   }
+                 }
+                 
+               }`
+
+        cy.request({
+
+            method: 'POST',
+            url: 'https://graphql-weather-api.herokuapp.com/',
+            body: {
+              query: queryString
+            },
+        
+        
+    }).then(response => {
+        cy.log(response.body)
+    
+    })
+
+    
+   })
+
+})
